741d061d249ef66d846cc316ef129c8aabd9a79e
Joshimello <joshualeanjw@gmail.com>
2023-11-18 12:42:16 +0800

    fix: loader ui

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -32,13 +32,11 @@
   </C.SideNavItems>
 </C.SideNav>
 
-
-{#if $navigating}
-<div class="h-100vh w-full flex items-center justify-center bg-[#FCFAFC]" transition:fade={{ duration: 200, delay: 100 }}>
-  <img class="h-50" src="/loader.gif" alt="" />
-</div>
-{:else}
 <C.Content>
+  {#if $navigating}
+  <div class="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-white z-99" transition:fade={{ duration: 100 }}>
+    <img class="h-50" src="/loader.gif" alt="" />
+  </div>
+  {/if}
   <slot/>
 </C.Content>
-{/if}
