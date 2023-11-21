9cbc5a651262a30aaaa7c7247045125a1aeac230
Joshimello <joshualeanjw@gmail.com>
2023-11-21 21:34:09 +0800

    admin response batch actions

--- src/routes/admin/[slug]/responses/+page.server.ts
+++ src/routes/admin/[slug]/responses/+page.server.ts
@@ -1,14 +1,54 @@
 import { redirect, fail } from '@sveltejs/kit';
 
 export const load = async ({ locals, params }) => {
-  let records
+  let records, statuses
   try {
     records = await locals.pb.collection(params.slug).getFullList({
       expand: 'responder,status'
     })
+    statuses = await locals.pb.collection('statuses').getFullList()
   }
   catch (err) {
     throw redirect(302, '/admin')
   }
-  return { records }
+  return { records, statuses }
+}
+
+export const actions = {
+  change: async ({ locals, request, params }) => {
+    const data = await request.formData();
+    const status = data.get('status')
+
+    // try {
+    //   const record = await locals.pb.collection(params.slug).update(params.resp, {
+    //     status: status
+    //   });
+    //   return fail(400, { success: true });
+    // }
+    // catch (err) {
+    //   return fail(400, { success: false });
+    // }
+  },
+  mail: async ({ locals, request, params }) => {
+    const data = await request.formData();
+    const email = data.get('email')
+    const subject = data.get('subject')
+    const content = data.get('content');
+    const replyto = data.get('replyto');
+    const mailas = data.get('mailas')
+
+    // try {
+    //   const mailid = await locals.rs.emails.send({
+    //     from: mailas,
+    //     to: email,
+    //     subject: subject,
+    //     html: content,
+    //     reply_to: replyto
+    //   })
+    //   return fail(400, { success: true });
+    // }
+    // catch (err) {
+    //   return fail(400, { success: false });
+    // }
+  }
 }
--- src/routes/admin/[slug]/responses/+page.svelte
+++ src/routes/admin/[slug]/responses/+page.svelte
@@ -3,29 +3,35 @@
   import * as C from 'carbon-components-svelte';
   import { format } from 'timeago.js'
   import Launch from "carbon-icons-svelte/lib/Launch.svelte"
+  import Editor from '$lib/Editor.svelte'
 
   export let data, form
-  $: ({ id, name, questions, idcount, idprefix, records } = data)
+  $: ({ id, name, questions, idcount, idprefix, records, mailas, replyto, statuses } = data)
 
   let selectedRowIds = []
+  let mailopen = false, statusopen = false
+  let mailcontent, selectedStatus
 </script>
 
 <div class="flex flex-col gap-8">
   <h2>Viewing {name} responses</h2>
 
   <C.DataTable
+    sortable
     batchSelection
     bind:selectedRowIds
     headers={[
       { value: 'Serial ID', key: 'serial' },
+      { value: 'Title', key: '0' },
       { value: 'Created', key: 'created' },
       { value: 'Updated', key: 'updated' },
-      { value: 'Status', key: 'expand' },
-      { value: '', key: 'id' },
+      { value: 'Status', key: 'expand', sort: (a, b) => a.status[0].title.localeCompare(b.status[0].title) },
+      { key: 'rid', empty: true },
+      { key: 'overflow', empty: true },
     ]}
-    rows={records}>
+    rows={records.map((i, idx) => ({ ...i, rid: i.id, id: idx }))}>
     <svelte:fragment slot="cell" let:row let:cell>
-      {#if cell.key == 'id'}
+      {#if cell.key == 'rid'}
       <C.Link icon={Launch} href={'/admin/' + id + '/responses/' + cell.value}>
         Details
       </C.Link>
@@ -35,16 +41,28 @@
       {format(cell.value)}
       {:else if cell.key == 'expand'}
       <span class="font-bold" style:color={cell.value.status[0].color}>{cell.value.status[0].title}</span>
+      {:else if cell.key == 'overflow'}
+      <C.OverflowMenu flipped>
+        <C.OverflowMenuItem/>
+        <C.OverflowMenuItem/>
+        <C.OverflowMenuItem/>
+      </C.OverflowMenu>
       {:else}
         {cell.value}
       {/if}
     </svelte:fragment>
     <C.Toolbar>
       <C.ToolbarBatchActions>
-        <C.Button>Mail selected</C.Button>
+        <C.Button on:click={() => statusopen = true}>Set status of selected</C.Button>
+        <C.Button on:click={() => mailopen = true}>Mail to selected</C.Button>
       </C.ToolbarBatchActions>
       <C.ToolbarContent>
-        <C.ToolbarSearch />
+        <C.ToolbarSearch
+          shouldFilterRows={(item, value) => {
+            let searchVal = idprefix + item.serial.padStart(3, '0') + item[0]
+            return searchVal.toLowerCase().includes(value.toLowerCase())
+          }}
+        />
         <C.ToolbarMenu>
           <C.ToolbarMenuItem primaryFocus>A</C.ToolbarMenuItem>
           <C.ToolbarMenuItem>B</C.ToolbarMenuItem>
@@ -56,3 +74,42 @@
   </C.DataTable>
 
 </div>
+
+<C.Modal
+  bind:open={statusopen}
+  modalHeading="Setting status of"
+  passiveModal
+>
+  <span>{selectedRowIds.map(i => idprefix + records[i].serial.padStart(3, '0')).join(', ')}</span>
+  <form action="?/mail" method="POST">
+    <input value={selectedStatus} name="status" class="hidden" />
+    <C.Dropdown
+      class="mt-4 mb-16"
+      bind:selectedId={selectedStatus}
+      items={statuses}
+      itemToString={(item) => {
+        return item.title
+      }}
+    />
+    <C.Button type="submit">Change status</C.Button>
+  </form>
+</C.Modal>
+
+<C.Modal
+  bind:open={mailopen}
+  modalHeading="Sending mail"
+  passiveModal
+>
+  <form action="?/mail" method="POST">
+    <C.TextInput name="email" value={[...(new Set(selectedRowIds.map(i => records[i].expand.responder[0].email)))].join(', ')} placeholder="Receiver" readonly />
+    <C.TextInput name="subject" placeholder="Subject" />
+    <Editor placeholder="Content" bind:value={mailcontent} />
+    <hr class="border-gray-500">
+    <C.TextInput name="mailas" value={mailas} inline labelText="Will send as:" readonly/>
+    <C.TextInput name="replyto" placeholder="Reply To" value={replyto} inline labelText="Will reply to:" />
+    <div class="flex justify-end pt-4">
+      <input class="hidden" value={mailcontent} name="content" />
+      <C.Button type="submit" kind="primary">Send</C.Button>
+    </div>
+  </form>
+</C.Modal>
