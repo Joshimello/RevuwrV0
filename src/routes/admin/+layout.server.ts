6da7b0f21dee8dbd7dc961c04b9da5993c91362b
Joshimello <joshualeanjw@gmail.com>
2024-01-21 11:52:31 +0800

    empty

--- src/routes/admin/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect, fail } from '@sveltejs/kit';
-
-export const load = async ({ locals, params }) => {
-  const records = await locals.pb.collection('applications').getFullList({
-    fields: 'id,name'
-  })
-  return { records }
-}
